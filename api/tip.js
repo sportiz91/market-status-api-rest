a31942c0732170413092d4e4237e986f835088c4
Santiago Pablo Ortiz <sportiz9109@gmail.com>
2022-04-04 13:49:54 -0300

    implement http connection for endpoint 1

--- api/tip.js
+++ api/tip.js
@@ -1,6 +1,11 @@
+// Requiring server related:
 const express = require("express");
 const WebSocket = require("ws");
-const streamPrice = require("../helpers/streamPrice");
+
+// Requiring external packages:
+const axios = require("axios");
+
+// Requiring helpers:
 const reducer = require("../helpers/reducer");
 const mapSorting = require("../helpers/mapSorting");
 const finder = require("../helpers/finder");
@@ -12,87 +17,119 @@ const router = express.Router();
 // @access   Public
 router.post("/", async (req, res) => {
   const pair = req.body.pairTip;
+  const flag = req.body.realTip;
 
-  console.log(pair);
+  if (flag === "One") {
+    const baseUrl = "https://api-pub.bitfinex.com/v2";
+    const pathParams = `book/${pair}/P0`;
+    const queryParams = "len=1";
 
-  const webSocket = new WebSocket("ws://localhost:5000");
+    const aggUrl = `${baseUrl}/${pathParams}?${queryParams}`;
 
-  webSocket.onopen = () => {
-    console.log("Web Socket Code on tip.js");
+    const queryResult = await axios.get(aggUrl);
+    const arrayResult = queryResult.data;
 
-    // webSocket.send("Hello from Client!");
-    webSocket.send(pair);
+    console.log("arrayResult:");
+    console.log(arrayResult);
 
-    webSocket.on("message", (msg) => {
-      console.log(`Server says: ${msg}`);
+    const responseBid = {
+      price: arrayResult[0][0],
+      amount: arrayResult[0][2],
+    };
 
-      const recreatedBook = JSON.parse(msg);
-      console.log(recreatedBook);
+    const responseAsk = {
+      price: arrayResult[1][0],
+      amount: arrayResult[1][2],
+    };
 
-      // Sanatizing order book:
-      const keyValueArrayBids = Object.entries(recreatedBook.bids);
-      const keyValueArrayAsks = Object.entries(recreatedBook.asks);
+    const responseAgg = {
+      bestBid: responseBid,
+      bestAsk: responseAsk,
+    };
 
-      console.log("keyValueArrayBids:");
-      console.log(keyValueArrayBids);
+    res.json(responseAgg);
+  }
 
-      console.log("keyValueArrayAsks:");
-      console.log(keyValueArrayAsks);
+  if (flag === "Real") {
+    const webSocket = new WebSocket("ws://localhost:5000");
 
-      const bidsArray = reducer(keyValueArrayBids);
-      const asksArray = reducer(keyValueArrayAsks);
+    webSocket.onopen = () => {
+      console.log("Web Socket Code on tip.js");
 
-      console.log("bidsArray:");
-      console.log(bidsArray);
+      // webSocket.send("Hello from Client!");
+      webSocket.send(pair);
 
-      console.log("asksArray:");
-      console.log(asksArray);
+      webSocket.on("message", (msg) => {
+        console.log(`Server says: ${msg}`);
 
-      const orderedBidsArray = mapSorting(bidsArray, "bids");
-      const orderedAsksArray = mapSorting(asksArray, "asks");
+        const recreatedBook = JSON.parse(msg);
+        console.log(recreatedBook);
 
-      console.log("orderedBidsArray:");
-      console.log(orderedBidsArray);
+        // Sanatizing order book:
+        const keyValueArrayBids = Object.entries(recreatedBook.bids);
+        const keyValueArrayAsks = Object.entries(recreatedBook.asks);
 
-      console.log("orderedAsksArray:");
-      console.log(orderedAsksArray);
+        console.log("keyValueArrayBids:");
+        console.log(keyValueArrayBids);
 
-      const bestBid = orderedBidsArray[0].toString();
-      const bestAsk = orderedAsksArray[0].toString();
+        console.log("keyValueArrayAsks:");
+        console.log(keyValueArrayAsks);
 
-      console.log("bestBid:");
-      console.log(bestBid);
+        const bidsArray = reducer(keyValueArrayBids);
+        const asksArray = reducer(keyValueArrayAsks);
 
-      console.log("bestAsk:");
-      console.log(bestAsk);
+        console.log("bidsArray:");
+        console.log(bidsArray);
 
-      const foundBid = finder(bestBid, bidsArray);
-      const foundAsk = finder(bestAsk, asksArray);
+        console.log("asksArray:");
+        console.log(asksArray);
 
-      console.log("foundBid:");
-      console.log(foundBid);
+        const orderedBidsArray = mapSorting(bidsArray, "bids");
+        const orderedAsksArray = mapSorting(asksArray, "asks");
 
-      console.log("foundAsk:");
-      console.log(foundAsk);
+        console.log("orderedBidsArray:");
+        console.log(orderedBidsArray);
 
-      const responseBid = {
-        price: foundBid[1].price,
-        amount: foundBid[1].amount,
-      };
+        console.log("orderedAsksArray:");
+        console.log(orderedAsksArray);
 
-      const responseAsk = {
-        price: foundAsk[1].price,
-        amount: foundAsk[1].amount,
-      };
+        const bestBid = orderedBidsArray[0].toString();
+        const bestAsk = orderedAsksArray[0].toString();
 
-      const responseAgg = {
-        bestBid: responseBid,
-        bestAsk: responseAsk,
-      };
+        console.log("bestBid:");
+        console.log(bestBid);
 
-      res.json(responseAgg);
-    });
-  };
+        console.log("bestAsk:");
+        console.log(bestAsk);
+
+        const foundBid = finder(bestBid, bidsArray);
+        const foundAsk = finder(bestAsk, asksArray);
+
+        console.log("foundBid:");
+        console.log(foundBid);
+
+        console.log("foundAsk:");
+        console.log(foundAsk);
+
+        const responseBid = {
+          price: foundBid[1].price,
+          amount: foundBid[1].amount,
+        };
+
+        const responseAsk = {
+          price: foundAsk[1].price,
+          amount: foundAsk[1].amount,
+        };
+
+        const responseAgg = {
+          bestBid: responseBid,
+          bestAsk: responseAsk,
+        };
+
+        res.json(responseAgg);
+      });
+    };
+  }
 });
 
 module.exports = router;
